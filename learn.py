aa1e32faefc2e5783a84be2ba3669fe018576d77
noramyer <nmyer2015@gmail.com>
2018-11-29 20:55:17 -0500

    train frequency table method

--- learn.py
+++ learn.py
@@ -1,9 +1,17 @@
 import load_data
 import numpy as np
 
+""" The cuisine is a frequency table which holds the real counts of ingredients in each
+cuisine type based on recipes that will be used in Naive Bayes analysis
+            indian  american    greek
+turmeric      3         1         0
+tomato        2         2         1
+"""
+cuisine_map = {}
+cuisine_total_counts = {}
 
-def encodeRecipes():
-    dataset = load_data.loadDataSet('train.json')
+def encodeRecipes(jsonFile):
+    dataset = load_data.loadDataSet(jsonFile)
 
     recipesList = dataset['ingredients']
     ingredientsList = list(dataset['ingredientsSet'])
@@ -27,6 +35,30 @@ def encodeRecipes():
 def writeRecipes(encodedRecipes):
     np.savetxt("recipe_arrays.txt", encodedRecipes, fmt="%s")
 
+def naiveBayes():
+    trainData("train.json")
+    test_data = encodeRecipes("test.json")
+
+
+def trainData(jsonFile):
+    recipeData = loadJson(jsonFile)
+
+    for recipe in recipeData:
+        cuisine_type = recipe["cuisine"]
+        for i in recipe["ingredients"]:
+            if not i in cuisine_map:
+                cuisine_map[i] = {}
+                cuisine_map[i]["total"] = 0
+
+            if not cuisine_type in cuisine_map[i]:
+                cuisine_map[i][cuisine_type] = 0
+
+            if not cuisine_type in cuisine_total_counts:
+                cuisine_total_counts[cuisine_type] = 0
+
+            cuisine_map[i][cuisine_type] += 1
+            cuisint_map[i]["total"] +=1
+            cuisine_total_counts[cuisine_type] +=1
 
 def loadRecipes():
     encodedRecipes = np.loadtxt("recipe_arrays.txt")
